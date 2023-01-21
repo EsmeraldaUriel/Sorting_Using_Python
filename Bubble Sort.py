f2b701189538e59bfe30663f5304b10073f8474c
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2023-01-21 18:38:40 +0800

    "creating for loop for the given list"

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,5 +1,10 @@
 def bubbleSort(arr):
     n = len(arr)
 
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            print("j",j)
+            print("j+1", j+1)
+            print(arr)
 
 

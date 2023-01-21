596fc9c7ca584ea02fd973c6903c6f04865267c3
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2023-01-21 19:12:58 +0800

    "creating condition on the element or list that will be separate to each other the left and right"

--- Merge Sort.py
+++ Merge Sort.py
@@ -1,2 +1,7 @@
 def mergeSort(myList):
+    if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+        print("left: ", left, "right: ", right)
 
